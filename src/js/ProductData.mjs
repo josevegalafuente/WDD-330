85a2b2dba50ff2ae279dfc1a9469bffcf7f2850b
josevegalafuente <jvegalafuente@byupathway.edu>
2026-05-29 00:57:04 -0400

    Expand inventory with dynamic category listings

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,6 +1,8 @@
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+function convertToJson(response) {
+  if (response.ok) {
+    return response.json();
   } else {
     throw new Error("Bad Response");
   }
@@ -9,15 +11,16 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+  async getData(category) {
+    const response = await fetch(`${baseURL}products/search/${category}`);
+    const data = await convertToJson(response);
+
+    return data.Result;
   }
+
   async findProductById(id) {
-    const products = await this.getData();
+    const products = await this.getData(this.category);
     return products.find((item) => item.Id === id);
   }
 }
--- src/js/product.js
+++ src/js/product.js
@@ -2,8 +2,10 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
 const productId = getParam("product");
+const category = getParam("category") || "tents";
+
+const dataSource = new ProductData(category);
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
