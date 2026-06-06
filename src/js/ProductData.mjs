af85bbf4e79ea5ec8bd68a129bb936626d9a9135
josevegalafuente <jvegalafuente@byupathway.edu>
2026-06-06 16:27:15 -0400

    Add checkout process

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,9 +9,10 @@ function convertToJson(response) {
 }
 
 export default class ProductData {
-  constructor(category) {
+  constructor(category = "") {
     this.category = category;
   }
+
   async getData(category) {
     const response = await fetch(`${baseURL}products/search/${category}`);
     const data = await convertToJson(response);
@@ -22,5 +23,20 @@ export default class ProductData {
   async findProductById(id) {
     const products = await this.getData(this.category);
     return products.find((item) => item.Id === id);
+  }
+
+  async checkout(order) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    };
+
+    const response = await fetch(`${baseURL}checkout/`, options);
+    const data = await convertToJson(response);
+
+    return data;
   }
 }
